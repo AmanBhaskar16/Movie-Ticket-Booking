d3ecb3c2bbcaec55ee4cc58e8111e7b0d0472e11
AmanBhaskar16 <amanbhaskar16@gmail.com>
2025-06-17 12:32:28 +0530

    Added movies details

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
         <Link onClick={()=> {scrollTo(0,0);setIsOpen(false)}} to='/movies'>Movies</Link>
         <Link onClick={()=> {scrollTo(0,0);setIsOpen(false)}} to='/'>Theaters</Link>
         <Link onClick={()=> {scrollTo(0,0);setIsOpen(false)}} to='/'>Releases</Link>
-        <Link onClick={()=> {scrollTo(0,0);setIsOpen(false)}} to='/favourite'>Favourites</Link>
+        <Link onClick={()=> {scrollTo(0,0);setIsOpen(false)}} to='/favourite'>Favourite</Link>
       </div>
 
       <div className='flex items-center gap-8'>
