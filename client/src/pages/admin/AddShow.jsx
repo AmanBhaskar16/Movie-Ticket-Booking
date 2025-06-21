59a23d98eafea08f3f11a28e96c3a10b4f46c151
AmanBhaskar16 <amanbhaskar16@gmail.com>
2025-06-21 07:27:19 +0530

    Added Admin dashboard

---
+++ client/src/pages/admin/AddShow.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Title from '../../components/admin/Title'
+
+const AddShow = () => {
+  return (
+    <div>
+      <Title text1={"Add"} text2={"Shows"} />
+    </div>
+  )
+}
+
+export default AddShow
---
+++ client/src/pages/admin/ListBookings.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Title from '../../components/admin/Title'
+
+const ListBookings = () => {
+  return (
+    <>
+      <Title text1={"All"} text2={"Bookings"} />
+    </>
+  )
+}
+
+export default ListBookings
